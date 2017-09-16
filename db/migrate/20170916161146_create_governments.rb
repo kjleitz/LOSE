49ce506369d2ca7ae585d343dfc11330c0a75c56
Keegan Leitz <keegan@openbay.com>
2017-09-16 12:34:37 -0400

    make 'em prettier

--- db/migrate/20170916161146_create_governments.rb
+++ db/migrate/20170916161146_create_governments.rb
@@ -1,10 +1,11 @@
 class CreateGovernments < ActiveRecord::Migration[5.1]
   def change
     create_table :governments do |t|
-      t.string :type
-      t.integer :freedom_axis
-      t.integer :equality_axis
-      t.belongs_to :person, foreign_key: true, optional: true
+      t.string     :type
+      t.text       :description
+      t.integer    :freedom_axis
+      t.integer    :equality_axis
+      t.belongs_to :person,       foreign_key: true, optional: true
 
       t.timestamps
     end

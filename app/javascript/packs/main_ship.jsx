3c8230c20a1bc54d8341b5724a28881bf0a83b91
Keegan Leitz <keegan@openbay.com>
2017-10-18 09:06:21 -0400

    better flame... yeah this shouldn't be in this branch

--- app/javascript/packs/main_ship.jsx
+++ app/javascript/packs/main_ship.jsx
@@ -11,6 +11,7 @@ export default class MainShip extends React.Component {
           position:        'fixed',
           top:             'calc(50% - 15px)',
           left:            'calc(50% - 15px)',
+          zIndex:          '10',
           width:           '30px',
           height:          '30px',
           borderRadius:    '15px 15px 0 0',
--- app/javascript/packs/ship.jsx
+++ app/javascript/packs/ship.jsx
@@ -4,20 +4,20 @@ export default class Ship extends React.Component {
   render() {
     let xFlame = "0px";
     let yFlame = "0px";
-    if (this.props.moveDirection === "left")     xFlame = "20px";
-    if (this.props.moveDirection === "right")    xFlame = "-20px";
-    if (this.props.moveDirection === "forward")  yFlame = "20px";
-    if (this.props.moveDirection === "backward") yFlame = "-20px";
+    if (this.props.moveDirection === "left")     xFlame = "10px";
+    if (this.props.moveDirection === "right")    xFlame = "-10px";
+    if (this.props.moveDirection === "forward")  yFlame = "15px";
     const flameStyle = this.props.moveDirection === '' ? {} : {
-      boxShadow: `${xFlame} ${yFlame} 5px -2px orangered`
+      boxShadow: `${xFlame} ${yFlame} 5px -3px orangered`
     }
 
     return (
-      // <div style={this.props.style}></div>
       <div style={this.props.style}>
         <div style={_.extend({
           width:  "100%",
-          height: "100%",
+          height: "calc(100% - 15px)",
+          marginTop: "15px",
+          zIndex: '0',
         }, flameStyle)}></div>
       </div>
     )
